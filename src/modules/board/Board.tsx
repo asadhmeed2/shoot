6bbc2223bb151d04d811e572920746276839db49
asaadhmeedrunn <asad.hmeed@runnableweb.com>
2022-11-30 09:05:11 +0200

    show the array on the screen

--- src/modules/board/Board.tsx
+++ src/modules/board/Board.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import cn from "classnames";
 
 import styles from "./Board.module.scss";
 
 export const Board = () => {
+  const boardBoxes = useMemo(
+    () =>
+      [...Array(1000)].map((box, idx) => {
+        return (
+          <div className={cn(styles.box, `col-${idx} flex-center`)}>{idx}</div>
+        );
+      }),
+    []
+  );
   return (
-    <div className="flex-wrap h-100">
-      {new Array(1000).map((box, idx) => {
-        return <div className={cn(styles.box, `col-${idx}`)}>{idx}</div>;
-      })}
+    <div className={cn("flex-wrap w-100 h-100 flex-center", styles.container)}>
+      {boardBoxes}
     </div>
   );
 };
